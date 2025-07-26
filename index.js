5f64e0ce4c6ba54e5dfcdd7b13e9b5f765e598dd
wilmercp28 <wilmercp28@gmail.com>
2025-07-26 14:52:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import { Buffer } from 'buffer';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
+
 
 app.use(express.json());
 
